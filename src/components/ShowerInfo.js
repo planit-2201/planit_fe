a1c86ce4d19f8b043d51d195ee4b4e24a90891ef
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-10 12:53:27 -0600

    Listen for changes to seconds or minutes and update state accordingly

--- src/components/ShowerInfo.js
+++ src/components/ShowerInfo.js
@@ -1,7 +1,8 @@
 import '../styles/ShowerInfo.css'
 import { useStopwatch } from 'react-timer-hook';
+import { useEffect } from 'react';
 
-const ShowerInfo = () => {
+const ShowerInfo = ({ todaysSeconds, todaysMinutes, setTodaysSeconds, setTodaysMinutes }) => {
 
   const {
     seconds,
@@ -14,28 +15,16 @@ const ShowerInfo = () => {
     reset,
   } = useStopwatch({ autoStart: false });
 
+  useEffect(() => {
+    setTodaysSeconds(seconds)
+  }, [seconds])
+
+  useEffect(() => {
+    setTodaysMinutes(minutes)
+  }, [minutes])
+
   return (
     <section className='info-container'>
-      <div className='shower-info-text'>
-        <h1 className='shower-info-title'>Need Help Shortening Your Time?</h1>
-        <ul className='link-list'>
-          <li className='link'>some link or tip</li>
-          <li className='link'>some link or tip</li>
-          <li className='link'>some link or tip</li>
-        </ul>
-      </div>
-      <div className='shower-data'>
-        <div className='shower-data-text-box'>
-          <h3 className='shower-data-text'>Average</h3>
-          <h3 className='shower-data-text'>Today</h3>
-          <h3 className='shower-data-text'>Yesterday</h3>
-        </div>
-        <div className='shower-data-num-box'>
-          <h3 className='shower-data-num'>6:19</h3>
-          <h3 className='shower-data-num'>5:26</h3>
-          <h3 className='shower-data-num'>7:12</h3>
-        </div>
-      </div>
       <div style={{textAlign: 'center'}}>
         <p>Your Shower Timer</p>
         <div style={{fontSize: '100px'}}>
@@ -45,8 +34,23 @@ const ShowerInfo = () => {
         <button onClick={pause}>Pause</button>
         <button onClick={reset}>Reset</button>
       </div>
+      <div className='shower-data'>
+        <div className='shower-data-text-box'>
+          <h3 className='shower-data-text'>Average</h3>
+          <h3 className='shower-data-text'>Today</h3>
+          <h3 className='shower-data-text'>Yesterday</h3>
+        </div>
+        <div className='shower-data-num-box'>
+          <h3 className='shower-data-num'>6:19</h3>
+          <h3 className='shower-data-num'>{todaysMinutes}:{todaysSeconds}</h3>
+          <h3 className='shower-data-num'>7:12</h3>
+        </div>
+      </div>
     </section>
   )
 }
+
+
+// Add button to record time, or auto post at midnight?
 
 export default ShowerInfo;
