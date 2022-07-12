03e11364f526b898cad589fcda59e820547bcecc
Nikhila Balmoori <nbalmoori@gmail.com>
2022-07-12 14:28:11 -0600

    Set up Cypress Testing stubs

--- cypress/e2e/exampleTest.cy.js
+++ cypress/e2e/exampleTest.cy.js
@@ -1,8 +1,51 @@
-describe ('sample test', () => {
+import { aliasQuery, aliasMutation, hasOperationName } from '../utils/graphql-test-utils'
 
-  it('Should be able to pass a test', () => {
-    cy.visit('http://localhost:3000/')
-    cy.url('http://localhost:3000/')
+describe ('Dashboard', () => {
+
+  beforeEach(() => {
+    cy.intercept('POST', 'https://immense-hollows-78338.herokuapp.com/graphql', (req) => {
+
+      aliasQuery(req, 'getUser')
+      aliasMutation(req, 'createDailyRecord')
+    })
+    .visit('http://localhost:3000/')
   })
-  
+
+  it('Should display homepage information', () => {
+    cy.intercept('POST', 'https://immense-hollows-78338.herokuapp.com/graphql', (req) => {
+      const { body } = req
+      if (hasOperationName(req, 'getUser')) {
+        // Declare the alias from the initial intercept in the beforeEach
+        req.alias = 'gqlgetUserQuery'
+
+        // Set req.fixture or use req.reply to modify portions of the response
+        req.reply((res) => {
+          // Modify the response body directly
+          res.body.data.getUser.weeklyAverageShowerTime = 1000
+          res.body.data.getUser.weeklyAverageWaterUsage = 1000
+          res.body.data.getUser.thirtydayAverageShowerTime = 1000
+        })
+      }
+    })
+    cy.visit('http://localhost:3000/')
+
+
+
+
+  })
+
 })
+
+
+
+  // it('Should display shower timer', () => {
+  //
+  // })
+  //
+  // it('Should display item counters', () => {
+  //
+  // })
+  //
+  // it('Should be able to submit a daily record', () => {
+  //
+  // })
