14e184be168adc0454d636be1432d3d41440d28a
Nikhila Balmoori <nbalmoori@gmail.com>
2022-07-12 15:00:16 -0600

    Add test for dashboard initial display

--- cypress/e2e/exampleTest.cy.js
+++ cypress/e2e/exampleTest.cy.js
@@ -23,29 +23,30 @@ describe ('Dashboard', () => {
           // Modify the response body directly
           res.body.data.getUser.weeklyAverageShowerTime = 1000
           res.body.data.getUser.weeklyAverageWaterUsage = 1000
-          res.body.data.getUser.thirtydayAverageShowerTime = 1000
+          res.body.data.getUser.thirtydayAverageShowerTime = 2000
         })
       }
     })
     cy.visit('http://localhost:3000/')
-
-
-
+    cy.contains('Did You Know...')
+    cy.contains('The average American uses 5,336 gallons of water on showers per year?')
+    cy.contains('If the average shower was 5 minutes long, that number would drop to 3650!')
+    cy.get('.main-info-pic').should('have.attr', 'src').should('include', '../assets/save-water-pic.jpeg')
+    cy.contains('Your Shower Timer')
+    cy.get('.timer-btn-container').children().should('have.length', 3)
+    cy.get('.timer-btn-container').contains('Start')
+    cy.get('.timer-btn-container').contains('Pause')
+    cy.get('.timer-btn-container').contains('Reset')
+    cy.contains('Today')
+    cy.contains('7 Day Average')
+    cy.contains('30 Day Average')
+    cy.contains('0:0')
+    cy.contains('16:40')
+    cy.contains('33:20')
+    cy.contains('Single Use Containers')
+    cy.contains('Plastic Straws')
+    cy.contains('Plastic Shopping Bags')
+    //need to test for nav and timer functionality after bug fixes and submit daily record button functionality
 
   })
-
 })
-
-
-
-  // it('Should display shower timer', () => {
-  //
-  // })
-  //
-  // it('Should display item counters', () => {
-  //
-  // })
-  //
-  // it('Should be able to submit a daily record', () => {
-  //
-  // })
