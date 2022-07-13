22a606e4f40bb329d6cd5047dc294a79901bf5b1
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-13 08:19:21 -0600

    Fix small timer bugs

--- src/components/ShowerInfo.js
+++ src/components/ShowerInfo.js
@@ -46,6 +46,12 @@ const ShowerInfo = ({
     }
   }
 
+  const resetShower = () => {
+    setTotalSeconds(0)
+    setTotalMinutes(0)
+    reset();
+  }
+
 
   return (
     <section className='info-container'>
@@ -57,7 +63,7 @@ const ShowerInfo = ({
         <div className='timer-btn-container'>
           <button className='timer-btn' onClick={startShower}>Start</button>
           <button className='timer-btn' onClick={() => {pause(); setTotalShowerTime(todaysSeconds, todaysMinutes)}}>Pause</button>
-          <button className='timer-btn' onClick={() => {reset(); pause()}}>Reset</button>
+          <button className='timer-btn' onClick={() => {pause(); resetShower()}}>Reset</button>
         </div>
       </div>
       <div className='shower-data'>
@@ -67,7 +73,7 @@ const ShowerInfo = ({
           <h3 className='shower-data-text'>30 Day Average</h3>
         </div>
         <div className='shower-data-num-box'>
-          <h3 className='shower-data-num'>{totalMinutes}:{totalSeconds}</h3>
+          <h3 className='shower-data-num'>{todaysMinutes}:{todaysSeconds}</h3>
           <h3 className='shower-data-num'>{Math.floor(weeklyAverageShowerTime/60)}:{weeklyAverageShowerTime % 60}</h3>
           <h3 className='shower-data-num'>{Math.floor(thirtyDayAverageShowerTime/60)}:{thirtyDayAverageShowerTime % 60}</h3>
         </div>
@@ -79,5 +85,10 @@ const ShowerInfo = ({
 // If a user clicks start while the timer is already running, don't reset it.
   // Fixed - used the built in isRunning
 // As the timer changes, reflect the new state in the UI
+  // Fixed - had to JavaScript interpolate a different piece of state to reflect this
+// When the timer is reset, then the user clicks 'start' the time resets to the last time. Fix this 
+  // Fixed - had to add a helper function that resets the total minutes and seconds in state back to 0,
+  // Put the reset function inside of this helper function
+  // Had to change the order in which the two functions were invoked when the reset button is clicked
 
 export default ShowerInfo;
