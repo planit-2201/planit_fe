66e775fe38e849bc84e7567ecc04d032a65b723c
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-18 07:08:09 -0600

    Style 404 message

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom';
 import '../styles/Error.css';
 
 const Error = () => {
   return (
     <div className='fof-box'>
-      <p className='fof-message'>Oh no! This page doesn't exist!</p>
-      <Link to='/' className='redirect'><strong className='redirect'>Click here to go back to the main page.</strong></Link>
+      <p className='fof-message'>Oh no - this page doesn't exist! Navigate to an existing page using the menu above.</p>
     </div>
   );
 };
