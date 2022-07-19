5d0804f77221faf405ef8ead3896adbc49d8a92e
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-19 09:42:07 -0600

    Change syntax

--- src/components/ShowerMinInfo.js
+++ src/components/ShowerMinInfo.js
@@ -15,7 +15,7 @@ const ShowerMinInfo = ({ username, currentPage, setCurrentPage }) => {
       <p className="data-paragraph"></p>
       <p className="data-title">Tips to use less water:</p>
       <ul className="data-paragraph">
-        <li>Try your best to limit your shower to 5 minutes in length.</li>
+        <li>Try your best to limit your showers to 5 minutes in length.</li>
         <li>Consider investing in low-flow appliances, such as shower heads and toilets.</li>
         <li></li>
       </ul>
